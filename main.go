cd0ae77aef3209df8002b3a1d3600cece21a5ef4
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2024-02-09 03:33:33 -0800

    completed structs functions and loops

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"example.com/price-calulator/prices"
+)
 
 func main() {
-	var prices []float64 = []float64{10, 20, 30}
+	
 	var taxRates []float64 = []float64{0, 0.07, 0.1, 0.15}
 
 	var result map[float64][]float64 = make(map[float64][]float64)
 
 	for _, taxRate := range taxRates {
-		var taxIncludedPrices []float64 = make([]float64, len(prices))
-		for priceindex, price := range prices {
-			taxIncludedPrices[priceindex] = price * (1 + taxRate)
-		}
-		result[taxRate] = taxIncludedPrices
+		priceJob:= prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
 
 	fmt.Println(result)
